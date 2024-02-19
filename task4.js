5c2c7015925eabc5d7e1eb5753a9b0e56751816a
slefforge <57022538+slefforge@users.noreply.github.com>
2024-02-19 18:07:08 -0500

    n

---
+++ task4.js
@@ -0,0 +1,25 @@
+/*
+
+!!!!!!!! IMPORTANT !!!!!!!
+
+In this task, you are asked to submit a post to exploit the stored-XSS.
+In order to verify your code, we ask you to submit the code using this file.
+Please follow the format down below. 
+
+*/
+
+/*
+
+Your exploitable payload should be like below in a post.
+whaterver<script>X</script> whatever
+
+TODO: Please copy X into the coding section.
+
+NOTE: You can include comments in the coding section.
+
+*/
+
+/* Coding section start */
+
+
+/* Coding section end */
---
+++ task5_3.js
@@ -0,0 +1,25 @@
+/*
+
+!!!!!!!! IMPORTANT !!!!!!!
+
+In this task, you are asked to implement a tabnaabing attack
+In order to verify your code, we ask you to submit the code using this file.
+Please follow the format down below. 
+
+*/
+
+/*
+
+Your exploitable link should be like
+https://cs6262.gtisc.gatech.edu/vulnerable/endpoint/?whaterver<script>X</script> whatever
+
+TODO: Please copy X in the coding section
+
+NOTE: You can include comments in the coding section.
+
+*/
+
+/* Coding section start */
+
+
+/* Coding section end */
